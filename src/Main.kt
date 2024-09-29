8b2ec1b8fe08762c3116d7a6d5220b0e1ff6c3f7
Abdik <alma55497@gmail.com>
2024-09-30 00:35:17 +0300

    Initial commit 2

---
+++ src/Main.kt
@@ -0,0 +1,18 @@
+//val text = """
+//${'$'}_9.99
+//"""
+//val currentTemp: Float = 24.5f
+//
+//
+//fun main() {
+//    println(text)
+//}
+fun main() {
+    val a: Int = 1000
+    val b: String = "log message"
+    val c: Double = 3.14
+    val d: Long = 100_000_000_000_000
+    val e: Boolean = false
+    val f: Char = '\n'
+    print(f)
+}
