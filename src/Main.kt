1805bbf0755038e6410d8511fdf8e4a3de6b0288
Abdik <alma55497@gmail.com>
2024-10-22 16:29:03 +0300

    +fun+lambda

--- src/Main.kt
+++ src/Main.kt
@@ -7,12 +7,25 @@
 //fun main() {
 //    println(text)
 //}
-fun main() {
-    val a: Int = 1000
-    val b: String = "log message"
-    val c: Double = 3.14
-    val d: Long = 100_000_000_000_000
-    val e: Boolean = false
-    val f: Char = '\n'
-    print(f)
-}
+
+//fun main() {
+//    val a: Int = 1000
+//    val b: String = "log message"
+//    val c: Double = 3.14
+//    val d: Long = 100_000_000_000_000
+//    val e: Boolean = false
+//    val f: Char = '\n'
+//    print(f)
+//}
+
+
+//fun main() {
+//    val actions = listOf("title", "year", "author")
+//    val prefix = "https://example.com/book-info"
+//    val id = 5
+//    val urls = actions.map { action -> "$prefix/$id/$action" }
+//    println(urls)
+//}
+
+
+
